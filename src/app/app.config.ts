3f99cffae808dbaf79abe12a6a1ede6a3e7f4b0a
Erick Jhunnior Huamán Cruz <huamancruzerick@gmail.com>
2025-11-21 17:58:29 -0500

    Configuración inicial de Firebase, AngularFire y ajustes en app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "gestion-turnos-barberia", appId: "1:988575885616:web:ac55714a227425dfcc40f5", storageBucket: "gestion-turnos-barberia.firebasestorage.app", apiKey: "AIzaSyArTg6WCyUwEf-bAJxZKsvhhGORdaTH15o", authDomain: "gestion-turnos-barberia.firebaseapp.com", messagingSenderId: "988575885616", projectNumber: "988575885616", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
