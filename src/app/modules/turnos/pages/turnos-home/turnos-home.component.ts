de40d3552ad9e4ab183a7da700ee69be8681dec5
Erick Jhunnior Huamán Cruz <huamancruzerick@gmail.com>
2025-11-28 19:28:47 -0500

    feat(turnos): agreggue el  módulo Turnos con TurnosHome y rutas iniciales

---
+++ src/app/modules/turnos/pages/turnos-home/turnos-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-turnos-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './turnos-home.component.html',
+  styleUrls: ['./turnos-home.component.css'] // ← YA CORREGIDO
+})
+export class TurnosHomeComponent {}
---
+++ src/app/modules/turnos/turnos.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TurnosHomeComponent } from './pages/turnos-home/turnos-home.component';
+
+export const TURNOS_ROUTES: Routes = [
+  {
+    path: '',
+    component: TurnosHomeComponent
+  }
+];
