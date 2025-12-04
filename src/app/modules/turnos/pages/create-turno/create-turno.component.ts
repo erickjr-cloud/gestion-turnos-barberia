721be03799a97bef82eec7413acf756510efb662
Erick Jhunnior Huamán Cruz <huamancruzerick@gmail.com>
2025-12-04 11:31:17 -0500

    feat(turnos): formulario reactivo básico para crear turnos

--- src/app/modules/turnos/pages/create-turno/create-turno.component.ts
+++ src/app/modules/turnos/pages/create-turno/create-turno.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-create-turno.component',
-  imports: [],
+  selector: 'app-create-turno',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-turno.component.html',
-  styleUrl: './create-turno.component.css',
+  styleUrls: ['./create-turno.component.css']
 })
 export class CreateTurnoComponent {
 
+  turnoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.turnoForm = this.fb.group({
+      cliente: ['', Validators.required],
+      fecha: ['', Validators.required],
+      hora: ['', Validators.required],
+      servicio: ['', Validators.required],
+      notas: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.turnoForm.invalid) {
+      console.log('Formulario incompleto');
+      return;
+    }
+
+    console.log('Turno listo para guardar:', this.turnoForm.value);
+  }
 }
