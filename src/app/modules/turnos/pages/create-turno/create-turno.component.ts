6a34b90e9d8cde53f29ed5e652f87fcc3ac1dc50
Erick Jhunnior Huamán Cruz <huamancruzerick@gmail.com>
2025-12-04 11:27:43 -0500

    feat(turnos): agregada la ruta /turnos/crear para crear turnos

---
+++ src/app/modules/turnos/pages/create-turno/create-turno.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-turno.component',
+  imports: [],
+  templateUrl: './create-turno.component.html',
+  styleUrl: './create-turno.component.css',
+})
+export class CreateTurnoComponent {
+
+}
--- src/app/modules/turnos/turnos.routes.ts
+++ src/app/modules/turnos/turnos.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { TurnosHomeComponent } from './pages/turnos-home/turnos-home.component';
+import { CreateTurnoComponent } from './pages/create-turno/create-turno.component';
 
 export const TURNOS_ROUTES: Routes = [
   {
     path: '',
     component: TurnosHomeComponent
+  },
+  {
+    path: 'crear',
+    component: CreateTurnoComponent
   }
 ];
