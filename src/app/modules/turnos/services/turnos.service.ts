a2bc9ea7680db2d087747d30eda9045c3031a534
Erick Jhunnior Huamán Cruz <huamancruzerick@gmail.com>
2025-12-04 11:07:51 -0500

    fix(turnos): inicializar turnosRef en el constructor para evitar error TS2729

--- src/app/modules/turnos/services/turnos.service.ts
+++ src/app/modules/turnos/services/turnos.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, collectionData, addDoc, doc, updateDoc, deleteDoc } from '@angular/fire/firestore';
+import { Turno } from '../interfaces/turno.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TurnosService {
 
-  constructor() { }
+  private turnosRef: any;
+
+  constructor(private firestore: Firestore) {
+    this.turnosRef = collection(this.firestore, 'turnos');
+  }
 
 }
