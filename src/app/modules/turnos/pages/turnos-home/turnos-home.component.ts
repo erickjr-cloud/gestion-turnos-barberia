587a133cfd5871f3582d52e59384c6fc7ee97e67
Erick Jhunnior Huamán Cruz <huamancruzerick@gmail.com>
2025-12-08 17:13:48 -0500

    feat: dashboard básico en module turnos

--- src/app/modules/turnos/pages/turnos-home/turnos-home.component.ts
+++ src/app/modules/turnos/pages/turnos-home/turnos-home.component.ts
@@ -1,15 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';   // ← IMPORTANTE
+import { RouterModule } from '@angular/router';
+import { TurnosService } from '../../services/turnos.service';
+import { Observable, map } from 'rxjs';
+import { Turno } from '../../interfaces/turno.interface';
 
 @Component({
   selector: 'app-turnos-home',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule      // ← NECESARIO para routerLink y router-outlet
+    RouterModule
   ],
   templateUrl: './turnos-home.component.html',
   styleUrls: ['./turnos-home.component.css']
 })
-export class TurnosHomeComponent {}
+export class TurnosHomeComponent implements OnInit {
+
+  turnos$!: Observable<Turno[]>;
+
+  totalTurnos = 0;
+  pendientes = 0;
+  completados = 0;
+  cancelados = 0;
+
+  constructor(private turnosService: TurnosService) {}
+
+  ngOnInit(): void {
+    this.turnos$ = this.turnosService.getTurnos();
+
+    this.turnos$.pipe(
+      map(turnos => {
+
+        this.totalTurnos = turnos.length;
+
+        this.pendientes = turnos.filter(t => t.estado === 'pendiente').length;
+        this.completados = turnos.filter(t => t.estado === 'confirmado').length;
+        this.cancelados = turnos.filter(t => t.estado === 'cancelado').length;
+
+        return turnos;
+      })
+    ).subscribe();
+  }
+}
