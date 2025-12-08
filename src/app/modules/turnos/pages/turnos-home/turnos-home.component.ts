82c0751ced8f48abd039af7fdae87fc78cb4748b
Erick Jhunnior Huamán Cruz <huamancruzerick@gmail.com>
2025-12-07 20:27:55 -0500

    feat(turnos): mostrar lista de turnos desde Firestore en TurnosHome

--- src/app/modules/turnos/pages/turnos-home/turnos-home.component.ts
+++ src/app/modules/turnos/pages/turnos-home/turnos-home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TurnosService } from '../../services/turnos.service';
+import { Turno } from '../../interfaces/turno.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-turnos-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './turnos-home.component.html',
-  styleUrls: ['./turnos-home.component.css'] // ← YA CORREGIDO
+  styleUrls: ['./turnos-home.component.css']
 })
-export class TurnosHomeComponent {}
+export class TurnosHomeComponent implements OnInit {
+
+  turnos$!: Observable<Turno[]>;
+
+  constructor(private turnosService: TurnosService) {}
+
+  ngOnInit(): void {
+    this.turnos$ = this.turnosService.getTurnos();
+  }
+}
